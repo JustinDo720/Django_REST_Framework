7a9d5f98037b736b8619966737436d197a082661
Do <justindo720@gmail.com>
2020-06-08 01:31:58 -0400

    Modified settings to add a default permission and used router for urls.py

--- Dog_programmers_app/urls.py
+++ Dog_programmers_app/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+from .views import DogViewSet, ProgrammingLanguageViewSet
+from .models import Dog, ProgrammingLanguage
+
+router = routers.DefaultRouter()
+router.register(Dog, DogViewSet)
+router.register(ProgrammingLanguage, ProgrammingLanguageViewSet)
 
 urlpatterns = [
+    path('api-info/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', views.index, name='index')
 ]
