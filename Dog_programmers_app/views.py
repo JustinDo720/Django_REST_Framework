b3324e2f5670622250d984c72c2f756cf0654e55
Do <justindo720@gmail.com>
2020-06-08 01:18:40 -0400

    Created Dog and Programming Language Viewsets

--- Dog_programmers_app/views.py
+++ Dog_programmers_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Dog, ProgrammingLanguage
+from .serializer import DogSerializer, ProgrammingLanguageSerializer
 
 # Create your views here.
+# Base views
 
 
 def index(request):
     return render(request, 'index.html')
+
+# View sets
+
+
+class DogViewSet(viewsets.ModelViewSet):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+
+class ProgrammingLanguageViewSet(viewsets.ModelViewSet):
+    queryset = ProgrammingLanguage.objects.all()
+    serializer_class = ProgrammingLanguageSerializer
+
