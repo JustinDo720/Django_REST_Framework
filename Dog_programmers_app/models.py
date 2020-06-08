e4102d21ac08082b7fbe2b9f7b77b98aaade8f24
Do <justindo720@gmail.com>
2020-06-08 00:57:40 -0400

    Created ProgrammingLanuage model and added it to the admin site

--- Dog_programmers_app/models.py
+++ Dog_programmers_app/models.py
@@ -5,3 +5,13 @@ from django.db import models
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ProgrammingLanguage(models.Model):
+    languages_names = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.languages_names
